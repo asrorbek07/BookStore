0b18eeefa04a751de8ff979cb2d2c8f09a98ff70
muhammadrasul099 <muhammadrasulafzalov@gmail.com>
2025-07-11 11:52:10 -0700

    Authormenu created

--- src/main/java/uz/kruz/menu/AuthorMenu.java
+++ src/main/java/uz/kruz/menu/AuthorMenu.java
@@ -7,81 +7,84 @@ import uz.kruz.util.TalkingAt;
 import java.util.Scanner;
 
 public class AuthorMenu {
-	//
+    //
 
-	private Scanner scanner;
-	private Narrator narrator;
+    private Scanner scanner;
+    private Narrator narrator;
 
-	public AuthorMenu() {
-		//
-		this.scanner = new Scanner(System.in);
-		this.narrator = new Narrator(this, TalkingAt.Left);
-	}
+    public AuthorMenu() {
+        //
+        this.scanner = new Scanner(System.in);
+        this.narrator = new Narrator(this, TalkingAt.Left);
+    }
 
-	public void show() {
-		//
-		int inputNumber = 0;
+    public void show() {
+        //
+        int inputNumber = 0;
 
-		while (true) {
-			displayMenu();
-			inputNumber = selectMenu();
+        while (true) {
+            displayMenu();
+            inputNumber = selectMenu();
 
-			switch (inputNumber) {
-			//
-			case 1:
-				narrator.sayln("New author added");
-				break;
-			case 2:
-				narrator.sayln("Author name is edited or updated");
-				break;
-			case 3:
-				narrator.sayln("Author deleted from table");
-				break;
-			case 4:
-				narrator.sayln("Showing all authors names");
-				break;
-			case 0:
-				this.exitProgram();
+            switch (inputNumber) {
+                case 1:
+                    narrator.sayln("New author added");
+                    break;
+                case 2:
+                    narrator.sayln("Author name is edited or updated");
+                    break;
+                case 3:
+                    narrator.sayln("Author deleted from table");
+                    break;
+                case 4:
+                    narrator.sayln("Showing all authors names");
+                    break;
+                case 5:
+                    narrator.sayln("All authors deleted");
+                    break;
+                case 0:
+                    this.exitProgram();
 
-			default:
-				narrator.sayln("Choose again!");
-			}
-		}
-	}
+                default:
+                    narrator.sayln("Choose again!");
+            }
+        }
+    }
 
-	private void displayMenu() {
-		//
-		narrator.sayln("");
-		narrator.sayln("..............................");
-		narrator.sayln(" Author menu ");
-		narrator.sayln("..............................");
-		narrator.sayln("1. Add new author");
-		narrator.sayln("2. Edit author name");
-		narrator.sayln("3. Delete author");
-		narrator.sayln("4. Show all authors");
-		narrator.sayln("..............................");
-		narrator.sayln(" 0. Exit program");
-		narrator.sayln("..............................");
-	}
+    private void displayMenu() {
+        //
+        narrator.sayln("");
+        narrator.sayln("..............................");
+        narrator.sayln(" Author menu ");
+        narrator.sayln("..............................");
+        narrator.sayln("1. Add new author");
+        narrator.sayln("2. Edit author name");
+        narrator.sayln("3. Delete author");
+        narrator.sayln("4. Show all authors");
+        narrator.sayln("5. Delete all authors");
+        narrator.sayln("..............................");
+        narrator.sayln(" 0. Exit program");
+        narrator.sayln("..............................");
+    }
 
-	private int selectMenu() {
-		//
-		narrator.say("Select: ");
-		int menuNumber = scanner.nextInt();
+    private int selectMenu() {
+        //
+        narrator.say("Select: ");
+        int menuNumber = scanner.nextInt();
 
-		if (menuNumber >= 0 && menuNumber <= 4) {
-			scanner.nextLine();
-			return menuNumber;
-		} else {
-			narrator.sayln("It's a invalid number --> " + menuNumber);
-			return -1;
-		}
-	}
-	
-	private void exitProgram() {
-		//
-		narrator.sayln("Program exit. Bye...");
-		scanner.close();
-		System.exit(0);
-	}	
+        if (menuNumber >= 0 && menuNumber <= 4) {
+            scanner.nextLine();
+            return menuNumber;
+        } else {
+            narrator.sayln("It's a invalid number --> " + menuNumber);
+            return -1;
+        }
+    }
+
+    private void exitProgram() {
+        //
+        narrator.sayln("Program exit. Bye...");
+        scanner.close();
+        System.exit(0);
+    }
 }
