24a57cbe076d9da7a505960ad11b683fad232be5
asrorbekobidjonov <asrorbek@nextree.io>
2025-07-15 11:11:14 +0500

    implement user retrieval in main class; replace BookStoreAdmin execution with UserRepositoryImpl usage

--- src/main/java/uz/kruz/Main.java
+++ src/main/java/uz/kruz/Main.java
@@ -1,5 +1,7 @@
 package uz.kruz;
 
+import uz.kruz.repository.impl.UserRepositoryImpl;
+
 /**
  * Main class for the Bookstore application
  */
@@ -7,7 +9,18 @@ public class Main {
 
     public static void main(String[] args) {
         // Create and run the BookStoreAdmin
-        BookStoreAdmin admin = new BookStoreAdmin();
-        admin.run();
+//        BookStoreAdmin admin = new BookStoreAdmin();
+//        admin.run();
+        UserRepositoryImpl userRepository = new UserRepositoryImpl();
+        userRepository.retrieveAll().forEach(
+                user -> {
+                    System.out.println("User ID: " + user.getId());
+                    System.out.println("User Name: " + user.getFullName());
+                    System.out.println("User Email: " + user.getEmail());
+                    System.out.println("User Role: " + user.getRole());
+                    System.out.println("-----------------------------");
+                }
+        );
+
     }
 }
