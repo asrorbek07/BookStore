35210cbf360afddb78256889814f680cdea23882
shokiriy <shokiriy18@gmail.com>
2025-07-15 12:22:07 +0500

    create implemented in OrderItemRepositoryImpl

--- src/main/java/uz/kruz/Main.java
+++ src/main/java/uz/kruz/Main.java
@@ -1,5 +1,7 @@
 package uz.kruz;
 
+import uz.kruz.repository.OrderItemRepository;
+import uz.kruz.repository.impl.OrderItemRepositoryImpl;
 import uz.kruz.repository.impl.UserRepositoryImpl;
 
 /**
@@ -11,13 +13,24 @@ public class Main {
         // Create and run the BookStoreAdmin
 //        BookStoreAdmin admin = new BookStoreAdmin();
 //        admin.run();
-        UserRepositoryImpl userRepository = new UserRepositoryImpl();
-        userRepository.retrieveAll().forEach(
-                user -> {
-                    System.out.println("User ID: " + user.getId());
-                    System.out.println("User Name: " + user.getFullName());
-                    System.out.println("User Email: " + user.getEmail());
-                    System.out.println("User Role: " + user.getRole());
+//        UserRepositoryImpl userRepository = new UserRepositoryImpl();
+//        userRepository.retrieveAll().forEach(
+//                user -> {
+//                    System.out.println("User ID: " + user.getId());
+//                    System.out.println("User Name: " + user.getFullName());
+//                    System.out.println("User Email: " + user.getEmail());
+//                    System.out.println("User Role: " + user.getRole());
+//                    System.out.println("-----------------------------");
+//                }
+//        );
+
+        OrderItemRepository orderItemRepository = new OrderItemRepositoryImpl();
+        orderItemRepository.retrieveAll().forEach(
+                orderItem -> {
+                    System.out.println("Order ID: " + orderItem.getId());
+                    System.out.println("Order Book ID: " + orderItem.getBookId());
+                    System.out.println("Order quantity: " + orderItem.getQuantity());
+                    System.out.println("Order price: " + orderItem.getPrice());
                     System.out.println("-----------------------------");
                 }
         );
--- src/main/java/uz/kruz/repository/impl/OrderItemRepositoryImpl.java
+++ src/main/java/uz/kruz/repository/impl/OrderItemRepositoryImpl.java
@@ -4,8 +4,7 @@ import uz.kruz.db.DatabaseConnection;
 import uz.kruz.domain.OrderItem;
 import uz.kruz.repository.OrderItemRepository;
 
-import java.sql.Connection;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,7 +22,22 @@ public class OrderItemRepositoryImpl implements OrderItemRepository {
 
     @Override
     public OrderItem create(OrderItem entity) {
-        throw new UnsupportedOperationException("Method not implemented");
+        String sql = "INSERT INTO order_items(order_id, book_id, quantity, price) VALUES(?, ?, ?, ?)";
+        try (PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            ps.setInt(1, entity.getOrderId());
+            ps.setInt(2, entity.getBookId());
+            ps.setInt(3, entity.getQuantity());
+            ps.setBigDecimal(4, entity.getPrice());
+            ps.executeUpdate();
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs.next()) {
+                entity.setId(rs.getInt(1));
+            }
+            return entity;
+        } catch (SQLException e) {
+            throw new UnsupportedOperationException("Method not implemented");
+        }
+
     }
 
     @Override
