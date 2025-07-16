3cd2155f59987c718c4732402a13fc476bc96747
MuhammmadazizKarimov <sharqiyturkiston@gmail.com>
2025-07-16 11:43:53 +0500

    AuthorRepositoryImpl is implemented.

--- src/main/java/uz/kruz/repository/impl/AuthorRepositoryImpl.java
+++ src/main/java/uz/kruz/repository/impl/AuthorRepositoryImpl.java
@@ -3,9 +3,10 @@ package uz.kruz.repository.impl;
 import uz.kruz.db.DatabaseConnection;
 import uz.kruz.domain.Author;
 import uz.kruz.repository.AuthorRepository;
+import uz.kruz.service.AuthorService;
 
-import java.sql.Connection;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,41 +24,124 @@ public class AuthorRepositoryImpl implements AuthorRepository {
 
     @Override
     public Author create(Author entity) {
-        throw new UnsupportedOperationException("Method not implemented");
+        String sql = "insert into author (full_name) values (?)";
+        try (PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            ps.setString(1,entity.getFullName());
+            ps.executeUpdate();
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs.next()){
+                entity.setId(rs.getInt(1));
+            }
+            return entity;
+
+        }catch(SQLException e){
+            throw new RuntimeException("Error creating entity",e);
+        }
+
     }
 
     @Override
     public Optional<Author> retrieveById(Integer id) {
-        throw new UnsupportedOperationException("Method not implemented");
+        String sql = "SELECT * FROM authors WHERE id = ? ";
+        try(PreparedStatement ps = connection.prepareStatement(sql)){
+            ps.setInt(1,id);
+            ResultSet rs = ps.executeQuery(sql);
+            if(rs.next()){
+                return rs.getLong(1);
+            }
+        }catch (SQLException e){
+            throw new RuntimeException("Error retrieving entity",e);
+        }
+        return 0;
     }
 
     @Override
-    public List<Author> retrieveAll() {
-        throw new UnsupportedOperationException("Method not implemented");
+    public List<Author> retrieveAll(String fullName) {
+        List<Author> authors = new ArrayList<>();
+        String sql = "SELECT * FROM authors";
+        try (Statement stms = connection.prepareStatement(sql)) {
+            ResultSet rs = stms.executeQuery(sql);
+            while (rs.next()) {
+                authors.add(mapRow(sql));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Error retrieving entity", e);
+        }
+        return authors;
+
     }
 
     @Override
     public boolean deleteById(Integer id) {
-        throw new UnsupportedOperationException("Method not implemented");
+        String sql  = "DELETE FROM authors WHERE id = ?";
+        try(PreparedStatement ps = connection.prepareStatement(sql)){
+            ps.setInt(1,id);
+            return ps.executeUpdate() > 0;
+        }catch (SQLException e){
+            throw new RuntimeException("Error deleting entity",e);
+        }
     }
 
     @Override
     public Author update(Author entity) {
-        throw new UnsupportedOperationException("Method not implemented");
+        String sql = "UPDATE authors SET full_name = ? WHERE id = ?";
+        try (PreparedStatement ps = connection.prepareStatement(sql)){
+            ps.setString(1,entity.getFullName());
+            ps.setInt(2, entity.getId());
+            ps.executeUpdate();
+            return entity;
+        }catch (SQLException e){
+            throw new RuntimeException("Error updating entity",e);
+        }
+
     }
 
     @Override
     public long count() {
-        throw new UnsupportedOperationException("Method not implemented");
+        String sql = "SELECT COUNT(*) FROM authors";
+        try (Statement stmt = connection.createStatement()){
+            ResultSet rs = stmt.executeQuery(sql);
+            if(rs.next()){
+                return rs.getLong(1);
+            }
+
+        }catch (SQLException e){
+            throw new RuntimeException("Error counting entity",e);
+        }
+        return 0;
     }
 
     @Override
     public List<Author> retrieveByName(String name) {
-        throw new UnsupportedOperationException("Method not implemented");
+        List<Author> authors = new ArrayList<>();
+        String  sql =  "SELECT * FROM authors WHERE full_name LIKE ?";
+        try(PreparedStatement ps = connection.prepareStatement(sql)){
+            ps.setString(1,"%" + name + "%");
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()){
+                authors.add(mapRow(rs));
+
+            }
+        }catch (SQLException e){
+            throw new RuntimeException("Error retrieving entity",e);
+        }
+
+        return authors;
     }
 
     @Override
     public List<Author> retrieveByBookId(Integer bookId) {
-        throw new UnsupportedOperationException("Method not implemented");
+
+        String sql = "SELECT * FROM authors WHERE book_id = ?";
+        try (PreparedStatement ps = connection.prepareStatement(sql)){
+            ps.setInt(1,bookId);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()){
+                return Optional.of(mapRow(rs));
+            }
+        }catch (SQLException e){
+            throw new RuntimeException("Error retrieving entity",e);
+        }
+        return Optional.empty();
     }
 }
