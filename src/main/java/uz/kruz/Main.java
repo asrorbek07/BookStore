dc5ae761b5d6dc5db3aac983322e88a674e27fb1
shokiriy <shokiriy18@gmail.com>
2025-07-16 11:16:32 +0500

    CRUD implemented in PublisherRepositoryImpl

--- src/main/java/uz/kruz/Main.java
+++ src/main/java/uz/kruz/Main.java
@@ -11,28 +11,16 @@ public class Main {
 
     public static void main(String[] args) {
         // Create and run the BookStoreAdmin
-//        BookStoreAdmin admin = new BookStoreAdmin();
-//        admin.run();
-//        UserRepositoryImpl userRepository = new UserRepositoryImpl();
-//        userRepository.retrieveAll().forEach(
-//                user -> {
-//                    System.out.println("User ID: " + user.getId());
-//                    System.out.println("User Name: " + user.getFullName());
-//                    System.out.println("User Email: " + user.getEmail());
-//                    System.out.println("User Role: " + user.getRole());
-//                    System.out.println("-----------------------------");
-//                }
-//        );
-
-        OrderItemRepository orderItemRepository = new OrderItemRepositoryImpl();
-        orderItemRepository.retrieveAll().forEach(
-                orderItem -> {
-                    System.out.println("Order ID: " + orderItem.getId());
-                    System.out.println("Order Book ID: " + orderItem.getBookId());
-                    System.out.println("Order quantity: " + orderItem.getQuantity());
-                    System.out.println("Order price: " + orderItem.getPrice());
+        BookStoreAdmin admin = new BookStoreAdmin();
+        admin.run();
+        UserRepositoryImpl userRepository = new UserRepositoryImpl();
+        userRepository.retrieveAll().forEach(
+                user -> {
+                    System.out.println("User ID: " + user.getId());
+                    System.out.println("User Name: " + user.getFullName());
+                    System.out.println("User Email: " + user.getEmail());
+                    System.out.println("User Role: " + user.getRole());
                     System.out.println("-----------------------------");
-
                 }
         );
 
--- src/main/java/uz/kruz/repository/impl/PublisherRepositoryImpl.java
+++ src/main/java/uz/kruz/repository/impl/PublisherRepositoryImpl.java
@@ -2,14 +2,23 @@ package uz.kruz.repository.impl;
 
 import uz.kruz.db.DatabaseConnection;
 import uz.kruz.domain.Publisher;
+import uz.kruz.repository.OrderItemRepository;
 import uz.kruz.repository.PublisherRepository;
 
-import java.sql.Connection;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 public class PublisherRepositoryImpl implements PublisherRepository {
+    private String INSERT = "INSERT INTO publishers(name, contact_email, phone) VALUES (?, ?, ?)";
+    private String SELECT = "SELECT * FROM publishers WHERE id = ?";
+    private String SELECT_ALL = "SELECT * FROM publishers";
+    private String DELETE = "DELETE FROM publishers WHERE id = ?";
+    private String UPDATE = "UPDATE publishers SET name = ?, contact_email = ?, phone = ? WHERE id = ?";
+    private String COUNT = "SELECT COUNT(*) FROM publishers";
+    private String SELECT_BY_NAME = "SELECT * FROM publishers WHERE name LIKE ?";
+    private String SELECT_BY_EMAIL = "SELECT * FROM publishers WHERE contact_email = ?";
 
     private final Connection connection;
 
@@ -23,41 +32,142 @@ public class PublisherRepositoryImpl implements PublisherRepository {
 
     @Override
     public Publisher create(Publisher entity) {
-        throw new UnsupportedOperationException("Method not implemented");
+
+        try(PreparedStatement ps = connection.prepareStatement(INSERT, Statement.RETURN_GENERATED_KEYS)) {
+            ps.setString(1, entity.getName());
+            ps.setString(2, entity.getContactEmail());
+            ps.setString(3, entity.getPhone());
+            ps.executeUpdate();
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs.next()) {
+                entity.setId(rs.getInt(1));
+            }
+        } catch (SQLException e) {
+            throw new UnsupportedOperationException("Method not implemented", e);
+        }
+        return entity;
     }
 
     @Override
     public Optional<Publisher> retrieveById(Integer id) {
-        throw new UnsupportedOperationException("Method not implemented");
+
+        try(PreparedStatement ps = connection.prepareStatement(SELECT)) {
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                return  Optional.of(mapRow(rs));
+            }
+        } catch (SQLException e) {
+            throw new UnsupportedOperationException("Method not implemented", e);
+        }
+        return Optional.empty();
     }
 
     @Override
     public List<Publisher> retrieveAll() {
-        throw new UnsupportedOperationException("Method not implemented");
+        List<Publisher> publishers = new ArrayList<>();
+
+        try(Statement stmt = connection.createStatement()) {
+            ResultSet rs = stmt.executeQuery(SELECT_ALL);
+            while (rs.next()) {
+                publishers.add(mapRow(rs));
+            }
+        } catch (SQLException e) {
+            throw new UnsupportedOperationException("Method not implemented", e);
+        }
+        return publishers;
     }
 
     @Override
     public boolean deleteById(Integer id) {
-        throw new UnsupportedOperationException("Method not implemented");
+
+        try(PreparedStatement ps = connection.prepareStatement(DELETE)) {
+            ps.setInt(1, id);
+            return ps.executeUpdate() > 0;
+        } catch (Exception e) {
+            throw new UnsupportedOperationException("Method not implemented", e);
+        }
     }
 
     @Override
     public Publisher update(Publisher entity) {
-        throw new UnsupportedOperationException("Method not implemented");
+
+        try(PreparedStatement ps = connection.prepareStatement(UPDATE)) {
+            ps.setString(1, entity.getName());
+            ps.setString(2, entity.getContactEmail());
+            ps.setString(3, entity.getPhone());
+            ps.setInt(4, entity.getId());
+            ps.executeUpdate();
+            return  entity;
+        } catch (Exception e) {
+            throw new UnsupportedOperationException("Method not implemented", e);
+        }
     }
 
     @Override
     public long count() {
-        throw new UnsupportedOperationException("Method not implemented");
+
+        try(Statement stmt = connection.createStatement()) {
+            ResultSet rs = stmt.executeQuery(COUNT);
+            if (rs.next()) {
+                return rs.getLong(1);
+            }
+        }catch (Exception e) {
+            throw new UnsupportedOperationException("Method not implemented");
+        }
+        return 0;
     }
 
     @Override
     public Optional<Publisher> retrieveByName(String name) {
-        throw new UnsupportedOperationException("Method not implemented");
+        try(PreparedStatement ps = connection.prepareStatement(SELECT_BY_NAME)) {
+            ps.setString(1, "%" + name + "%");
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                Optional.of(mapRow(rs));
+            }
+        } catch (Exception e) {
+            throw new UnsupportedOperationException("Method not implemented", e);
+        }
+        return Optional.empty();
     }
 
     @Override
     public List<Publisher> retrieveByEmail(String email) {
-        throw new UnsupportedOperationException("Method not implemented");
+        List<Publisher> publishers = new ArrayList<>();
+
+        try(PreparedStatement ps = connection.prepareStatement(SELECT_BY_EMAIL)) {
+            ps.setString(1, email);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                publishers.add(mapRow(rs));
+            }
+        } catch (Exception e) {
+            throw new UnsupportedOperationException("Method not implemented", e);
+        }
+        return publishers;
     }
+
+    private Publisher mapRow(ResultSet rs) throws SQLException {
+        Publisher publisher = new Publisher();
+        publisher.setId(rs.getInt("id"));
+        publisher.setName(rs.getString("name"));
+        publisher.setContactEmail(rs.getString("contact_email"));
+        publisher.setPhone(rs.getString("phone"));
+        return publisher;
+    }
+
+//    public static void main(String[] args) {
+//        PublisherRepository publisherRepository = new PublisherRepositoryImpl();
+//        publisherRepository.retrieveAll().forEach(
+//                publisher -> {
+//                    System.out.println("Publisher ID: " + publisher.getId());
+//                    System.out.println("Publisher Name: " + publisher.getName());
+//                    System.out.println("Publisher Email: " + publisher.getContactEmail());
+//                    System.out.println("Publisher Phone: " + publisher.getPhone());
+//                    System.out.println("-----------------------------");
+//
+//                }
+//        );
+//    }
 }
