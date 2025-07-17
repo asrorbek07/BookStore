6eee2f71cc2c656ee215ef19c94e7e72415937c0
MuhammmadazizKarimov <sharqiyturkiston@gmail.com>
2025-07-17 16:28:52 +0500

    AuthorServiceImpl is  implement AuthorService

--- src/main/java/uz/kruz/service/impl/AuthorServiceImpl.java
+++ src/main/java/uz/kruz/service/impl/AuthorServiceImpl.java
@@ -1,10 +1,15 @@
 package uz.kruz.service.impl;
 
 import uz.kruz.domain.Author;
+import uz.kruz.domain.User;
 import uz.kruz.dto.AuthorDTO;
 import uz.kruz.repository.AuthorRepository;
 import uz.kruz.service.AuthorService;
+import uz.kruz.util.StringUtil;
 
+
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,11 +23,24 @@ public class AuthorServiceImpl implements AuthorService {
 
     @Override
     public Author register(AuthorDTO dto) {
-        throw new UnsupportedOperationException("Method not implemented");
+        if (StringUtil.isEmpty(dto.getFullName())) {
+            throw new IllegalArgumentException("full name must not be empty");
+        }
+        if(authorRepository.retrieveByName(dto.getFullName())!=null){
+            throw new RuntimeException(String.format("Author with name %s already exists", dto.getFullName()));
+        }
+
+        Author author = Author.builder()
+                .fullName(dto.getFullName())
+                .build();
+
+
+        return authorRepository.create(author);
     }
 
     @Override
     public Optional<Author> findById(Integer id) {
+
         throw new UnsupportedOperationException("Method not implemented");
     }
 
@@ -38,7 +56,11 @@ public class AuthorServiceImpl implements AuthorService {
 
     @Override
     public Author modify(AuthorDTO dto, Integer id) {
-        throw new UnsupportedOperationException("Method not implemented");
+        Author author = authorRepository.retrieveById(id).orElseThrow(()-> new RuntimeException(String.format("Author with id %d does not exists", id)));
+        if(dto.getFullName()!=null && ! StringUtil.isEmpty(dto.getFullName())){
+            author.setFullName(dto.getFullName());
+        }
+        return authorRepository.update(author);
     }
 
     @Override
