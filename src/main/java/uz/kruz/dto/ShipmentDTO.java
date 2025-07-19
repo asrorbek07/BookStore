12bb0460bb461d3856a0d17522237963d6293012
shokiriy <shokir1898shs@gmail.com>
2025-07-19 18:58:35 +0500

    ShipmentServiceImpl implemented

--- src/main/java/uz/kruz/dto/ShipmentDTO.java
+++ src/main/java/uz/kruz/dto/ShipmentDTO.java
@@ -1,13 +1,21 @@
 package uz.kruz.dto;
 
+import com.sun.jna.platform.win32.Sspi;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
+
+import java.sql.Date;
+import java.sql.Timestamp;
 
 @EqualsAndHashCode(callSuper = true)
 @Getter
 @Setter
 @SuperBuilder
 public class ShipmentDTO extends BaseDTO {
+        private  Integer orderId;
+        private String trackingNo;
+        private Timestamp shippedAt;
+        private Date deliveryEstimate;
 }
