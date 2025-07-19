c204d0fbb64fe81a8aa092f5d02ec1001fb2f9ae
Xazratbek06 <rustamovhazratbek127@gmail.com>
2025-07-19 07:47:08 +0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,137 @@
+package UygaVazifa_24;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+//
+//public class Main {
+//    public static void main(String[] args) {
+
+//        LinkedList<String>students = new LinkedList<>();
+//
+//        students.add("Ali");        1-misol
+//        students.add("Vali");
+//        students.add("Hasan");
+//        students.add("Husan");
+//
+//        students.remove("Hasan");
+//
+//        System.out.println("Qolgan talabalar: " + students);
+//
+//        students.size();
+//
+//        System.out.println("Talabalar soni: " + students.size());
+//
+//
+//    }
+
+//        PhoneBook phoneBook = new PhoneBook();       2-misol
+//
+//        phoneBook.addContact("Ali", "998901234567");
+//        phoneBook.addContact("Vali", "998901234568");
+//        phoneBook.addContact("Hasan", "998901234569");
+//        phoneBook.addContact("Husan", "998901234570");
+//
+//        phoneBook.printContacts();
+//
+//        phoneBook.getPhone("Ali");
+//        phoneBook.getPhone("Vali");
+//
+//        phoneBook.removeContacts("Hasan");
+//        phoneBook.printContacts();
+//
+//        phoneBook.removeContacts("Husan");
+//        phoneBook.printContacts();
+//    }
+
+//        WordCollector wordCollector = new WordCollector();  // 3-misol
+//
+//        wordCollector.addWord("Java");
+//        wordCollector.addWord("Python");
+//        wordCollector.addWord("JavaScript");
+//        wordCollector.addWord("C++");
+//
+//        wordCollector.printWords();
+//    }
+
+//        LinkedList<Integer> sonlar = new LinkedList<>();  // 4-misol
+//
+//        sonlar.add(10);
+//        sonlar.add(20);
+//        sonlar.add(30);
+//        sonlar.add(40);
+//        sonlar.add(50);
+//
+//        System.out.println("Birinchi son: " + sonlar.getFirst());
+//
+//        System.out.println("Oxirgi son: " + sonlar.getLast());
+//
+//    }
+
+//            HashMap<String, String> phoneBook = new HashMap<>();    5-misol
+//
+//            phoneBook.put("Ali", "+998901234567");
+//
+//            String valiPhone = phoneBook.getOrDefault("Vali", "Not found");
+//
+//            System.out.println("Vali'ning raqami: " + valiPhone);
+//
+//    }
+//        HashSet<String> hayvonlar = new HashSet<>();      6-misol
+//
+//        hayvonlar.add("Sigir");
+//        hayvonlar.add("Tovuq");
+//        hayvonlar.add("It");
+//        hayvonlar.add("Qo'y");
+//
+//        hayvonlar.add("Tovuq"); // Takroriy qo'shilmaydi
+//
+//        System.out.println("Hayvonlar to'plami: " + hayvonlar);
+//
+//        HashMap<String,String>ismlar = new HashMap<>();  // 7-misol
+//
+//        ismlar.put("Ali", "Ali.@gmail.com");
+//        ismlar.put("Vali", "Vali.@gmail.com");
+//        ismlar.put("Hasan", "Hasan.@gmail.com");
+//
+//        ismlar.containsKey("Ali");
+//        ismlar.containsValue("Ali.@gmail.com");
+//
+//        System.out.println("Ali'ning emaili: " + ismlar.get("Ali"));
+
+//        LinkedList<String> cars = new LinkedList<>();     8-misol
+//
+//        cars.add("BMW");
+//        cars.add("Mercedes");
+//        cars.add("Audi");
+//        cars.add("Toyota");
+//
+//        cars.remove("Audi");
+//        cars.remove("Toyota");
+//
+//        System.out.println("Qolgan mashinalar: " + cars);
+
+//        HashMap<String,Integer>User = new HashMap<>();  // 9-misol
+//
+//        User.put("Karim", 25);
+//        User.put("Aziz", 30);
+//        User.put("Diyor", 28);
+//
+//        User.containsKey("Karim");
+//
+//        System.out.println("Karimning yoshi: " + User.get("Karim"));
+//
+//
+//        HashSet<String>kasblar = new HashSet<>();  // 10-misol
+//
+//        kasblar.add("Dasturchi");
+//        kasblar.add("Muhandis");
+//        kasblar.add("O'qituvchi");
+//        kasblar.add("Dasturchi"); // Takroriy qo'shilmaydi
+//        kasblar.add("San'atkor");
+//        kasblar.add("Muhandis"); // Takroriy qo'shilmaydi
+//
+//        System.out.println("Kasblar to'plami: " + kasblar);
+//    }
+//}
+
